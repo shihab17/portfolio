0ebf24523714d7ce95bcfc3bff747a31f8f528b6
shihab17 <shihabahmed620@gmail.com>
2021-05-08 03:22:02 +0600

    sending email succesfully

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,7 +4,7 @@ import './About.css';
 import Skills from '../Skills/Skills';
 const About = () => {
     return (
-        <div className="p-5">
+        <div className="p-5 my-bg">
             <div className="row about-header p-2 m-5 ">
                 <AboutHeader></AboutHeader>
             </div>
--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -2,12 +2,23 @@ import { faTelegramPlane } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import emailjs from 'emailjs-com';
 import './Contact.css';
 const Contact = (props) => {
     console.log(props)
     const sendEmail = e => {
         e.preventDefault();
-        console.log(e.target.value)
+        emailjs.sendForm('service_e50tz9s', 'template_8tn8hzj', e.target, 'user_HQkb3WLrWYNhNaXWaYby2')
+            .then((result) => {
+                console.log(result.text);
+                alert('Your message sent successfully ');
+                document.getElementById('name').value = '';
+                document.getElementById('email').value = '';
+                document.getElementById('subject').value = '';
+                document.getElementById('message').value = '';
+            }, (error) => {
+                console.log(error.text);
+            });
     }
     return (
         <section className="p-5 my-bg text-white my-bg">
@@ -23,20 +34,20 @@ const Contact = (props) => {
                 <div className="col-md-7">
                     <h3 className="p-3 my-color">Message Me</h3>
                     <hr />
-                    <form className="p-3 " action="" onSubmit={sendEmail}>
+                    <form className="p-3" action="" onSubmit={sendEmail}>
                         <div className="row">
                             <div className="col-12 col-md-6 form-group">
-                                <input className="form-control contact-input bg-dark" type="text" name="contact-name" id="contact-name" placeholder="Name" required />
+                                <input className="form-control contact-input bg-dark text-white" type="text" name="name" id="name"  placeholder="Name" required />
                             </div>
                             <div className="col-12 col-md-6 form-group">
-                                <input className="form-control contact-input bg-dark" type="email" name="contact-email" id="contact-name" placeholder="Email" required />
+                                <input className="form-control contact-input bg-dark text-white " type="email" name="email" id="email"  placeholder="Email" required />
                             </div>
                         </div>
                         <div className="col-12 form-group">
-                            <input className="form-control contact-input bg-dark" type="text" name="contact-subject" id="contact-subject" placeholder="Subject" required />
+                            <input className="form-control contact-input bg-dark text-white " type="text" name="subject" id="subject" placeholder="Subject" required />
                         </div>
                         <div className="col-12 form-group">
-                            <textarea className="form-control contact-input bg-dark" name="contact-message" id="contact-message" rows="5" placeholder="Message"></textarea>
+                            <textarea className="form-control contact-input bg-dark text-white " name="message" id="message" rows="5" placeholder="Message"></textarea>
                         </div>
                         <div className="col-12">
                             <button className="btn btn-lg my-btn p-3 text-white"> <FontAwesomeIcon icon={faTelegramPlane}></FontAwesomeIcon> Send</button>
